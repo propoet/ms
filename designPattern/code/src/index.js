a647e35623f32243dcbef7e7c030b0981dc73efc
propoet <propoet@163.com>
2019-12-30 16:10:13 +0800

    ''

--- designPattern/code/src/index.js
+++ designPattern/code/src/index.js
@@ -1,1 +1,21 @@
-class Person{}
+class Product {
+  constructor (name) {
+    this.name=name
+  }
+  init(){
+    alert('init')
+  }
+  fn1(){
+    alert('fn1')
+  }
+}
+class Creator{
+  create(name){
+    return new Product(name)
+  }
+}
+let creator=new Creator()
+
+let product = creator.create('ss');
+product.init()
+product.fn1()
