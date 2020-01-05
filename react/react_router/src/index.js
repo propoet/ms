5dd228606e4f822fe62a8c7435c5e937d9b2822e
propoet <propoet@163.com>
2020-01-05 14:16:22 +0800

    .

---
+++ react/react_router/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter,HashRouter} from 'react-router-dom'
+import App from './components/app'
+import './index.css'
+ReactDOM.render((<BrowserRouter>
+            <App/>
+        </BrowserRouter>),document.getElementById("root"))
+/*ReactDOM.render((<BrowserRouter>
+    <App/>
+</BrowserRouter>),document.getElementById('root'))*/
